0247cd3c587ba52320f6c40d22aee33991da69a5
martleru <68188509+martleru@users.noreply.github.com>
2024-04-15 16:32:08 +0300

    feat: add dark mode

---
+++ components/Providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { ReactNode } from "react";
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+const Providers = ({ children }: ProvidersProps) => {
+  return (
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300">
+        {children}
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
