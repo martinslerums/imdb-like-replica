ce6d8b204deaaa2b43b92c5118376efcad7985a6
martleru <68188509+martleru@users.noreply.github.com>
2024-04-16 14:29:23 +0300

    add: Added Loading effect

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <div className="flex justify-center mt-16">
+      <img className="h-52" src="loading.svg" alt="loading" />
+    </div>
+  )
+}
+
+export default LoadingPage
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,12 @@ const fetchMovies = async (genre: string) => {
   const response = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/movie/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    {
+      next: {
+        revalidate: 10000
+      }
+    }
   );
 
   const data = await response.json();
@@ -26,10 +31,7 @@ const fetchMovies = async (genre: string) => {
 
 const HomePage = async ({ searchParams }: HomePageProps) => {
   const genre = searchParams.genre || "fetchTrending";
-
   const results = await fetchMovies(genre);
-
-  console.log(results)
 
   return (
     <div>
