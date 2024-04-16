ba50480ebc2fd0aa38e3b03b8c2af68c418e4d2f
martleru <68188509+martleru@users.noreply.github.com>
2024-04-16 14:06:48 +0300

    add: Added little about page

---
+++ app/about/page.tsx
@@ -0,0 +1,31 @@
+const About = () => {
+  return (
+    <div className="max-w-7xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-500">About</h1>
+        <p>
+          Welcome to our movie-loving oasis, where technology meets passion in
+          the world of cinema. Fueled by Next.js, our project is a tribute to
+          the magic of film. With a deep-rooted love for movies guiding our
+          every move, we&apos;ve created a database akin to IMDb, meticulously
+          curated to cater to all tastes and preferences.
+        </p>
+        <p>
+          Why movies, you might ask? Simply put, because they&apos;re our passion.
+          From spine-tingling thrillers to heartwarming dramas, we&apos;ve immersed
+          ourselves in the captivating world of cinema, driven by its ability to
+          transport and inspire. Our project is more than just a collection of
+          titles—it&apos;s a labor of love, continuously evolving to provide the
+          ultimate movie-watching experience.
+        </p>
+        <p>
+          So whether you&apos;re a seasoned cinephile or a casual viewer, our
+          doors are open to you. Explore our database, rediscover old favorites,
+          and uncover hidden gems that await your discovery. Join us on a
+          journey through the magic of film, and let the movies ignite your
+          imagination once more.
+        </p>
+    </div>
+  );
+};
+
+export default About;
